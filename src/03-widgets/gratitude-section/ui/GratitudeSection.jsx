85915d6730a0aada5506fd3f9e2a74f9895f23af
Anatoly Shilyaev <Anatoly.Shilyaev@gmail.com>
2026-03-05 19:07:56 +0300

    change text

--- src/03-widgets/gratitude-section/ui/GratitudeSection.jsx
+++ src/03-widgets/gratitude-section/ui/GratitudeSection.jsx
@@ -4,11 +4,7 @@ export function GratitudeSection() {
   return (
     <section aria-label="Текстовое поздравление и пожелания" className={styles.gratitudeSection}>
       <div className={styles.quoteBlock}>
-        <p>Для просмотра новых поздравлений обновите страницу 😉</p>
-        {/* <p className={styles.quoteBlockAccent}>
-          Пусть в вашей жизни будет больше любви, уважения, поддержки и поводов для радости.
-        </p>
-        <p>Спасибо за ваш труд, заботу и выдержку. С 8&nbsp;Марта!</p> */}
+        <p>Для просмотра других поздравлений обновите страницу 😉</p>
       </div>
     </section>
   );
