9292ddac0feeed2c161593881c209900f2314e54
Anatoly Shilyaev <Anatoly.Shilyaev@gmail.com>
2026-03-05 18:53:41 +0300

    change text

--- src/03-widgets/page-footer/ui/PageFooter.jsx
+++ src/03-widgets/page-footer/ui/PageFooter.jsx
@@ -4,7 +4,7 @@ export function PageFooter() {
   return (
     <footer className={styles.footer}>
       <div>
-        <span className={styles.footer__noteStrong}>С уважением, мужчины Службы финансового уполномочнного.</span>
+        <span className={styles.footer__noteStrong}>С уважением, мужчины Службы финансового уполномоченного.</span>
       </div>
     </footer>
   );
